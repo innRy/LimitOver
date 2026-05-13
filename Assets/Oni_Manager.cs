6e6190d2470ce65e1baf9c0fffad1c50e65c0053
otara0720 <241205113@ccmailg.meijo-u.ac.jp>
2026-05-13 15:08:19 +0900

    demon

---
+++ Assets/Oni_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Oni_Manager : MonoBehaviour
+{
+    private GameObject player;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        player = GameObject.Find("ninngen");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.destination = player.transform.position;
+    }
+}
